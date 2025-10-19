5d05cefa9649afc8e6ec54f7d0ac7d3a6334c554
Ratmir Mukazhanov <ratmir.m2004@gmail.com>
2025-10-19 21:15:42 +0100

    Project initialized

---
+++ src/main/java/pt/estga/stonemark/StonemarkApplication.java
@@ -0,0 +1,13 @@
+package pt.estga.stonemark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StonemarkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StonemarkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pt/estga/stonemark/StonemarkApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.estga.stonemark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StonemarkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
