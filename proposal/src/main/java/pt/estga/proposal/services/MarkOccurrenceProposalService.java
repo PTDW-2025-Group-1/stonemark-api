c12e09d26dedd797e83d55a500f2e21735d3a221
Gustavo Gião <gustavogiao@ua.pt>
2026-01-06 17:29:44 +0000

    feat(proposal): add user statistics endpoint and DTO for MarkOccurrenceProposal

--- proposal/src/main/java/pt/estga/proposal/services/MarkOccurrenceProposalService.java
+++ proposal/src/main/java/pt/estga/proposal/services/MarkOccurrenceProposalService.java
@@ -2,6 +2,7 @@ package pt.estga.proposal.services;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import pt.estga.proposal.dtos.MarkOccurrenceProposalStatsDto;
 import pt.estga.proposal.entities.MarkOccurrenceProposal;
 import pt.estga.user.entities.User;
 
@@ -22,6 +23,8 @@ public interface MarkOccurrenceProposalService {
     MarkOccurrenceProposal update(MarkOccurrenceProposal proposal);
 
     void delete(MarkOccurrenceProposal proposal);
+
+    MarkOccurrenceProposalStatsDto getStatsByUser(User user);
 
     long countApprovedProposalsByUserId(Long userId);
 
